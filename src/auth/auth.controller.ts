2cb21f0fe6561e22536d6b176f1342f62e389670
Ahmed-M-Anis <ahmed.m.anis65@gmail.com>
2025-11-03 11:59:52 +0200

    comment useless  code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signUpDto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
-import { AuthGuard } from './guards/jwt.guard';
-import { RolesGuard } from 'src/auth/guards/role.guard';
-import { Role } from 'src/auth/decorator/role.decorator';
 import { loginDTO } from './dto/login.dto';
 import { ApiResponse } from 'src/common/utility/response/ApiResponse';
 import { PaginatedResponse } from 'src/common/utility/response/PaginatedResponse';
@@ -25,29 +11,9 @@ import { UserResponseDto } from './dto/user-response.dto ';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  /*   @Post()
-  async create(@Body() SignUpDto: SignUpDto) {
-    return await this.authService.create(SignUpDto);
-  }
-
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  } */
-
-  /*   @UseGuards(AuthGuard, RolesGuard)
-  @Role('user') */
+  //only for testing
+  /*  @UseGuards(AuthGuard, RolesGuard)
+  @Role('user') 
   @Get()
   async findAll() {
     const users = await this.authService.findAll();
@@ -74,17 +40,17 @@ export class AuthController {
       paginated.items,
       paginated.getMeta(),
     );
-  }
+  } */
 
   @Post('login')
   async logIn(@Body() loginDTO: loginDTO) {
     const user = await this.authService.logIn(loginDTO);
-    return user;
+    return ApiResponse.success('success', user);
   }
 
   @Post('signup')
   async signUp(@Body() SignUpDto: SignUpDto) {
     const user = await this.authService.signUp(SignUpDto);
-    return user;
+    return ApiResponse.success('success', user);
   }
 }
